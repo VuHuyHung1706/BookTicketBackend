11e71477d4a6e497648694f9f929cfc0cd739fb7
Vũ Huy Hùng <vuhuyhung17062002@gmail.com>
2025-08-06 22:08:47 +0700

    update project

--- src/main/java/com/web/backend/config/SecurityConfig.java
+++ src/main/java/com/web/backend/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
 
     private final String[] PUBLIC_ENDPOINTS = {
             "/accounts/register", "/auth/login", "/auth/introspect", "/auth/logout", "/auth/refresh", "/actors/**", "/genres/**" , "/payments/**",
-            "/auth/google/login",
+            "/auth/google/login", "accounts/send-otp", "/accounts/complete-register"
     };
 
     private final String[] PUBLIC_GET_ENDPOINTS = {
