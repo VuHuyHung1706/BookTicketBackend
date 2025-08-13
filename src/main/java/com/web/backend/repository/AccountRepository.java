251adbb5745be7df709a59fae0a7772946f4be76
Vũ Huy Hùng <vuhuyhung17062002@gmail.com>
2025-08-13 20:39:40 +0700

    update project

--- src/main/java/com/web/backend/repository/AccountRepository.java
+++ src/main/java/com/web/backend/repository/AccountRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface AccountRepository extends JpaRepository<Account, Integer> {
+public interface AccountRepository extends JpaRepository<Account, String> {
     boolean existsByUsername(String username);
     Optional<Account> findByUsername(String username);
 }
